2722e6366efdd97f16964a1096a263a68ab2032d
Martijn van der Wijst <martijn.van.der.wijst@philips.com>
2017-09-01 12:08:11 +0200

    add request example

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 'use strict';
 const express = require('express');
+const request = require('request');
+
 const app = express();
+const url ='https://requestb.in/uinu00ui';
+
+request(url, function (error, response, body) {
+  if (!error) {
+    console.log(body);
+  }
+});
 
 app.get('/', function (req, res) {
   res.send('Deze framboos is super vrolijk! :)');
